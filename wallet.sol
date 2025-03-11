24062e0e6c8d1a6897400341e49879700d239e9b
wWw <120179608+franc2es@users.noreply.github.com>
2025-03-11 16:28:18 +0800

    Update wallet.sol

--- wallet.sol
+++ wallet.sol
@@ -1,1 +1,11 @@
-
+function validate(UserOperation calldata userOp,) external override returns(){
+  require(_verifySignature(userOp, userOpHash), "Invalid signature");//验证签名
+  return 0;
+}
+function verifySignature()return(){
+  bytes32 message = keccak256(abi.encodePanked())//构造消息哈希
+  address signer = recover(message, )//恢复签名地址
+  require(signer == )//验证签名地址是否与用户钱包地址匹配
+}
+address signer = recover//从签名中恢复出签名者的地址
+require(signer ==)//验证恢复出的签名地址是否与 UserOperation 中的 sender 地址匹配
